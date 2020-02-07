e5e9cce04d0b10cb616f966d04f05ccb52aff59c
Sasaki Motoaki <voyegesansretour1@gmail.com>
2020-02-07 14:48:05 +0900

    devise導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    #omniauth_callbacks: 'users/omniauth_callbacks',
+  }
+
+  devise_scope :user do
+    get "users/:id", :to => "users/registrations#show"
+    get "sign_up", :to => "users/registrations#new"
+    get "users/:id/edit", :to => "users/registrations#edit"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+    get "users/:id/password_edit", :to => "users/passwords#edit"
+  end
+  root to: 'static_pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
