9579ec09f572c9613352c2a91602ff2ac388c956
Sasaki Motoaki <voyegesansretour1@gmail.com>
2020-02-07 17:09:08 +0900

    rails,deviseの日本語化

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
+
+  # def show
+  # end
 
   # GET /resource/sign_up
   # def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#home'
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
     passwords: 'users/passwords',
     #omniauth_callbacks: 'users/omniauth_callbacks',
   }
-
+  
   devise_scope :user do
-    get "users/:id", :to => "users/registrations#show"
-    get "sign_up", :to => "users/registrations#new"
-    get "users/:id/edit", :to => "users/registrations#edit"
-    get "login", :to => "users/sessions#new"
-    get "logout", :to => "users/sessions#destroy"
-    get "users/:id/password_edit", :to => "users/passwords#edit"
+    get "users/:id", :to => "users/registrations#show" # ユーザー個人ページ
+    get "sign_up", :to => "users/registrations#new" # 新規登録ページ
+    get "users/:id/edit", :to => "users/registrations#edit" # プロフィール編集ページ
+    get "login", :to => "users/sessions#new" # ログインページ
+    get "logout", :to => "users/sessions#destroy" # ログアウト
+    get "users/:id/password_edit", :to => "users/passwords#edit" # パスワード変更ページ
   end
-  root to: 'static_pages#home'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
