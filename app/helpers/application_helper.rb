bae98eb4e952e8e58e0ae6bd75ddf5c7f85b56f2
Sasaki Motoaki <voyegesansretour1@gmail.com>
2020-02-19 11:13:30 +0900

    applicationhelperspec作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title)
-    base_title = "BIGMatch"
+    base_title = "BGMatch"
     if page_title.blank?
       base_title
     else
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "ApplicationHelper" do
   include ApplicationHelper
-  describe "page title" do
-    let(:product) { create(:custom_product) }
+  describe "ページタイトル" do
+    let(:post) { create(:post) }
 
-    it "only display base_title" do
-      expect(full_title("")).to eq "BIGBAG Store"
+    it "base_titleのみを表示" do
+      expect(full_title("")).to eq "BGMatch"
     end
 
-    it "display full_title" do
-      expect(full_title(product.name)).to eq "Custom Product - BIGBAG Store"
+    it "full_titleを表示" do
+      expect(full_title(post.name)).to eq "MyString - BGMatch"
     end
   end
 end
