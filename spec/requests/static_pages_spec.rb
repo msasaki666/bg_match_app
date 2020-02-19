5930eae7896984ca8a8f9753b85a72389bcd2131
Sasaki Motoaki <voyegesansretour1@gmail.com>
2020-02-19 10:29:25 +0900

    Staticpagesのriquestspec作成

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,30 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
 
-  describe "GET #home" do
-    it "returns http success" do
-      get :home
-      expect(response).to have_http_status(:success)
+  describe "トップページ(staticpages#home)" do
+    let(:user) { create(:user) }
+
+    context "ログインしていない状態" do
+      before do
+        get root_path
+      end
+
+      it "リクエストが成功する" do
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "ログイン状態" do
+      before do
+        sign_in user
+        get root_path
+      end
+
+      it "リクエストが成功する" do
+        expect(response).to have_http_status(:success)
+      end
+
+      
     end
   end
-
 end
